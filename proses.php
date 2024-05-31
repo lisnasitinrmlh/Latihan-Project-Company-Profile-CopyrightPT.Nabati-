af634d82a7ec173c9d9bba04d6adb27b87d46253
lisnasitinrmlh <158829617+lisnasitinrmlh@users.noreply.github.com>
2024-05-31 08:28:38 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,17 @@
+<?php
+
+$email=$_POST['email'];
+$nama=$_POST['nama'];
+$penilaian=$_POST['penilaian'];
+$kritik=$_POST['kritik'];
+$saran=$_POST['saran'];
+
+echo "Terimakasih Telah Mengisi Quisioner!!!!!!"."<br>";
+echo "Email: ".$email."<br>";
+echo "Nama: ".$nama."<br>";
+echo "Penilaian: ".$penilaian."<br>";
+echo "Kritik: ".$kritik."<br>";
+echo "Saran: ".$saran."<br>";
+
+
+?>
